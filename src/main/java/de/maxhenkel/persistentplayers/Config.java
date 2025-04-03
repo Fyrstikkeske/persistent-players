974e2d5c5994cb189c1f949e2587b651561fe3ff
Fyrstikkeske <79742888+Fyrstikkeske@users.noreply.github.com>
2025-04-03 17:33:43 +0200

    jpost make it true

--- src/main/java/de/maxhenkel/persistentplayers/Config.java
+++ src/main/java/de/maxhenkel/persistentplayers/Config.java
@@ -6,13 +6,13 @@ public class Config {
 
     public static boolean persistCreativePlayers = true;
     public static boolean offlinePlayersSleep = false;
-    public static boolean betterLogging = false;
+    public static boolean betterLogging = true;
 
     public static void init(Configuration config) {
         config.load();
         persistCreativePlayers = config.getBoolean("persist_creative_players", "persistent_players", true, "If players in creative mode should persist");
         offlinePlayersSleep = config.getBoolean("offline_players_sleep", "persistent_players", false, "If players that are offline should be sleeping");
-        betterLogging = config.getBoolean("better_logging", "persistent_players", false, "For Better logging in case errors start happening");
+        betterLogging = config.getBoolean("better_logging", "persistent_players", true, "For Better logging in case errors start happening");
         config.save();
     }
 }
